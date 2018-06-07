29d799d18933895e83fa5b8b71d69173af347b4d
Jake Oliger <jakeoliger@gmail.com>
2018-06-07 12:30:13 +0300

    Adds en passant capturing and castling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/* INTERNAL TODO
+ * - Replace board representation with an array of only pieces and positions
+ *      Pros: Easier to iterate through pieces
+ *      Cons: Potentially more difficult to check if a space is occupied
+ * - Add isKingInCheck(king, board) method
+ * 
+ * 
+ */
+
 var [WHITE, BLACK] = [0, 1];
 var [W, B] = [WHITE, BLACK];
 var TEAM_NAME = [
@@ -25,28 +34,47 @@ var PIECE_NAME = [
     "King",
 ];
 
+/**
+ * Determines whether the attempted move is valid
+ *
+ * @param {*} start The start position of the move, also used to determine moving piece
+ * @param {*} end The end position of the move
+ * @param {*} board The array of piece positions on the board
+ * @returns
+ */
 function isValidMove(start, end, board) {
     var isValid = false;
     var mvmt = start - end;
     var piece = board[start];
+    var dir = mvmt > 0 ? 1 : -1;
+    var rowStart = start - start % 8;
+    var moves = [{
+        piece: piece,
+        start: start,
+        end: end
+    }];
+    var isCastling = false;
     switch (piece.type) {
         case PAWN:
             var maxMvmt = 8 * (piece.team === B ? 1 : -1);
             isValid = mvmt % 8 === 0 && (piece.team === B ? mvmt > 0 : mvmt < 0);
-            if (isValid && piece.isFirstMove) {
-                isValid = isValid && board[start - maxMvmt] === null;
-                maxMvmt *= 2;
+            if (isValid) {
+                if (piece.isFirstMove) {
+                    isValid = board[start - maxMvmt] === null;
+                    maxMvmt *= 2;
+                    piece.justDoubleMoved = true;
+                } else {
+                    piece.justDoubleMoved = false;
+                }
+                isValid = isValid && (piece.team === B ? mvmt <= maxMvmt : mvmt >= maxMvmt);
             }
-            isValid = isValid && (piece.team === B ? mvmt <= maxMvmt : mvmt >= maxMvmt);
             break;
         case ROOK:
-            var rowStart = start - start % 8;
-            var dir = mvmt > 0 ? 1 : -1;
             // Determine if the requested move matches a movement function for this piece
             var rookMoveFunc;
             if (mvmt % 8 === 0) {
                 rookMoveFunc = (i) => i + 8 * dir;
-            } else if (rowStart <= end && rowStart + 8 > end) {
+            } else if (rowStart <= end && rowStart + 7 >= end) {
                 rookMoveFunc = (i) => i + dir;
             } else {
                 isValid = false;
@@ -66,7 +94,6 @@ function isValidMove(start, end, board) {
             isValid = mvmt === 17 || mvmt === 15 || mvmt === 6 || mvmt === 10;
             break;
         case BISHOP:
-            var dir = mvmt > 0 ? 1 : -1;
             // Determine if the requested move matches a movement function for this piece
             var bishopMoveFunc;
             if (mvmt % 7 === 0) {
@@ -87,8 +114,6 @@ function isValidMove(start, end, board) {
             }
             break;
         case QUEEN:
-            var rowStart = start - start % 8;
-            var dir = mvmt > 0 ? 1 : -1;
             // Determine if the requested move matches a movement function for this piece
             var queenMoveFunc;
             if (mvmt % 7 === 0) {
@@ -97,7 +122,7 @@ function isValidMove(start, end, board) {
                 queenMoveFunc = (i) => i + 9 * dir;
             } else if (mvmt % 8 === 0) {
                 queenMoveFunc = (i) => i + 8 * dir;
-            } else if (rowStart <= end && rowStart + 8 > end) {
+            } else if (rowStart <= end && rowStart + 7 >= end) {
                 queenMoveFunc = (i) => i + dir;
             } else {
                 isValid = false;
@@ -113,28 +138,73 @@ function isValidMove(start, end, board) {
             }
             break;
         case KING:
-            mvmt = Math.abs(mvmt);
-            isValid = mvmt === 1 || mvmt === 7 || mvmt === 8 || mvmt === 9;
+            var absMvmt = Math.abs(mvmt);
+            var rowEnd = rowStart + 7;
+            isValid = absMvmt === 1 || absMvmt === 7 || absMvmt === 8 || absMvmt === 9;
+            if (absMvmt === 2 && piece.isFirstMove) {
+                // Attempted castling
+                var rookPosition = null;
+                if (dir > 0 && board[rowStart] !== null && board[rowStart].type === ROOK) {
+                    rookPosition = rowStart;
+                } else if (dir < 0 && board[rowEnd] !== null && board[rowEnd].type === ROOK) {
+                    rookPosition = rowEnd;
+                }
+                if (rookPosition !== null && board[rookPosition].isFirstMove) {
+                    isCastling = true;
+                    for (var i = rookPosition + dir; i !== start; i += dir) {
+                        if (board[i] !== null) {
+                            isCastling = false;
+                            break;
+                        }
+                    }
+                }
+                if (isCastling) {
+                    isValid = true;
+                    moves.push({
+                        piece: board[rookPosition],
+                        start: rookPosition,
+                        end: start - dir
+                    });
+                }
+            }
             break;
         default:
             isValid = false;
             break;
     }
     if (isValid) piece.isFirstMove = false;
-    return isValid;
+    return {isValid: isValid, moves: moves, isCastling: isCastling};
 }
 
+/**
+ * Determines whether a given move results in a valid vapture
+ *
+ * @param {*} validMove Whether the move is valid
+ * @param {*} start The starting position of the move
+ * @param {*} end The end position of the move
+ * @param {*} board The array of pieces on the board
+ * @returns The captured piece, if there is one
+ */
 function isValidCapture(validMove, start, end, board) {
     var captor = board[start];
-    var captive = board[end];
-    if (captor === null || captive === null) return false;
+    var captive = null;
+    var captivePos = end;
+    if (captor.type === PAWN && board[end] === null) {
+        // En passant capturing detection
+        captivePos += (captor.team === WHITE ? -8 : 8);
+    }
+    captive = board[captivePos];
+    if (captor === null || captive === null) return null;
     var isValid = false;
     var mvmt = start - end;
-    if (captor.team === captive.team) return false;
+    if (captor.team === captive.team) return null;
 
     switch (captor.type) {
         case PAWN:
-            if (validMove || (captor.team === B && mvmt < 0) || (captor.team === W && mvmt > 0)) {
+            if (validMove ||
+                (captor.team === B && mvmt < 0) ||
+                (captor.team === W && mvmt > 0) ||
+                (captivePos !== end && !captive.justDoubleMoved)) {
                 isValid = false;
                 break;
             }
@@ -152,7 +222,7 @@ function isValidCapture(validMove, start, end, board) {
             isValid = false;
             break;
     }
-    return isValid;
+    return (isValid ? captivePos : null);
 }
 
 class Pc {
@@ -160,6 +230,7 @@ class Pc {
         this.type = type;
         this.team = team;
         this.isFirstMove = true;
+        this.justDoubleMoved = false;
     }
 }
 
@@ -190,7 +261,7 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            spaces: [
+            /*spaces: [
                 new Pc(R, W), new Pc(KN, W), new Pc(BI, W), new Pc(KI, W), new Pc(Q, W), new Pc(BI, W), new Pc(KN, W), new Pc(R, W),
                 new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W),
                 null, null, null, null, null, null, null, null,
@@ -199,6 +270,16 @@ class Board extends React.Component {
                 null, null, null, null, null, null, null, null,
                 new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B),
                 new Pc(R, B), new Pc(KN, B), new Pc(BI, B), new Pc(Q, B), new Pc(KI, B), new Pc(BI, B), new Pc(KN, B), new Pc(R, B),
+            ],*/
+            spaces: [
+                new Pc(R, W), null, null, new Pc(KI, W), null, null, null, new Pc(R, W),
+                new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W), new Pc(P, W),
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null,
+                new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B), new Pc(P, B),
+                new Pc(R, B), new Pc(KN, B), null, null, new Pc(KI, B), new Pc(KN, B), null, new Pc(R, B),
             ],
             captured: [],
             desiredStart: null,
@@ -207,38 +288,53 @@ class Board extends React.Component {
         };
     }
 
+    cancelTurn() {
+        this.setState({desiredStart: null});
+    }
+
     handleClick(i) {
         var spaces = this.state.spaces.slice();
-        if (this.state.desiredStart === null) {
+        var mover = spaces[this.state.desiredStart];
+        var moverSpace = this.state.desiredStart;
+        if (moverSpace === null) {
             if (spaces[i] !== null && spaces[i].team === (this.state.whitesTurn ? WHITE : BLACK)) {
                 this.setState({
                     desiredStart: i,
                 });
             }
         } else {
-            var validMove = isValidMove(this.state.desiredStart, i, spaces);
-            var validCapture = isValidCapture(validMove, this.state.desiredStart, i, spaces);
-            var attemptedCapture = spaces[i] !== null;
-            
-            if (attemptedCapture && !validCapture) {
+            var move = isValidMove(moverSpace, i, spaces);
+            var capturedPiecePos = isValidCapture(move.isValid, moverSpace, i, spaces);
+            var successfulCapture = capturedPiecePos !== null;
+
+            console.log(move);
+
+            if (spaces[i] !== null && !successfulCapture && !move.isCastling) {
+                this.cancelTurn();
                 return;
             }
 
-            if (validMove || validCapture) {
+            if (move.isValid || successfulCapture) {
                 var newState = {whitesTurn: !this.state.whitesTurn || this.state.ignoreTurns};
-                if (spaces[i] !== null) {
+                if (successfulCapture) {
                     var captured = this.state.captured.slice();
-                    captured.push(spaces[i]);
+                    captured.push(spaces[capturedPiecePos]);
                     newState.captured = captured;
+                    spaces[capturedPiecePos] = null;
                 }
-                spaces[i] = spaces[this.state.desiredStart];
-                spaces[this.state.desiredStart] = null;
+
+                for (var i = 0; i < move.moves.length; i++) {
+                    var m = move.moves[i];
+                    console.log(m);
+                    spaces[m.end] = m.piece;
+                    spaces[m.start] = null;
+                }
     
                 newState.spaces = spaces;
                 newState.desiredStart = null;
                 this.setState(newState);
             } else {
-                this.setState({desiredStart: null});
+                this.cancelTurn();
             }
         }
     }
@@ -246,7 +342,7 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <p class="whos-turn">{this.state.whitesTurn ? "White's" : "Black's"} Turn</p>
+                <p className="whos-turn">{this.state.whitesTurn ? "White's" : "Black's"} Turn</p>
                 <table>
                     <tbody>
                     {[0, 1, 2, 3, 4, 5, 6, 7].map((x, i) => {
@@ -268,7 +364,7 @@ class Board extends React.Component {
                     })}
                     </tbody>
                 </table>
-                <h3 class="subtitle">Captured Pieces</h3>
+                <h3 className="subtitle">Captured Pieces</h3>
                 <ul id="capturedPieces">
                     {this.state.captured.map((piece, index) => {
                         return (
